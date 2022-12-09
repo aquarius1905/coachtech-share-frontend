1d939ec90ee7e0f5a1558aff2ccd993e91170f62
reiko_iguchi <reiko6122@gmail.com>
2022-12-09 20:16:18 +0900

    2022/12/09

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBNfrPO6vDEukC1vGSPEQGanBXKZiRH11o",
-  authDomain: "share-app-84535.firebaseapp.com",
-  projectId: "share-app-84535",
-  storageBucket: "share-app-84535.appspot.com",
-  messagingSenderId: "283196730351",
-  appId: "1:283196730351:web:41ab88920867e6b861e41a",
-  measurementId: "G-01CP687020"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: "AIzaSyBNfrPO6vDEukC1vGSPEQGanBXKZiRH11o",
+      authDomain: "share-app-84535.firebaseapp.com",
+      projectId: "share-app-84535",
+      storageBucket: "share-app-84535.appspot.com",
+      messagingSenderId: "283196730351",
+      appId: "1:283196730351:web:41ab88920867e6b861e41a",
+      measurementId: "G-01CP687020"
+    }
+  )
+}
+  
+export default firebase
